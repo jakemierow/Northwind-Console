6c15d5c0e5a58887e674db977a28a7aea43bb07a
Jake Mierow <jake.mierow@icloud.com>
2020-05-19 20:07:13 -0500

    * Northwind-Console.sln:
    * Product.cs:
    * Supplier.cs: added additional annotations

--- Northwind-Console/Models/Product.cs
+++ Northwind-Console/Models/Product.cs
@@ -1,16 +1,33 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace NorthwindConsole.Models
 {
     public class Product
     {
         public int ProductID { get; set; }
+
+       [Required(ErrorMessage = "Please enter a product name")]
         public string ProductName { get; set; }
         public string QuantityPerUnit { get; set; }
+
+        [Required(ErrorMessage = "Please enter a unit price")]
+        [RegularExpression(@"^\d+\.\d{0,2}$")]
+        [Range(-9999999999999999.99, 9999999999999999.99)]
         public decimal? UnitPrice { get; set; }
+
+        [Required(ErrorMessage = "Please enter units in stock")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter a valid number")]
         public Int16? UnitsInStock { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter a valid number")]
         public Int16? UnitsOnOrder { get; set; }
+
+        [Required(ErrorMessage = "Please enter the reorder level")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter a valid number")]
         public Int16? ReorderLevel { get; set; }
+
+        [Required(ErrorMessage = "Please enter Y or N")]
         public bool Discontinued { get; set; }
 
         public int? CategoryId { get; set; }
